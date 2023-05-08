7e5e1eb385eeac10016f04d4003256b1daec63a4
minntoko <minntoko0822@gmail.com>
2023-05-08 19:42:00 +0900

    add: Timelineのheaderを追加

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Timeline.css";
 
 function Timeline() {
   return (
-    <div className='timeline'>
+    <div className="timeline">
       {/* Header */}
-      <div className='timeline__header'>
-        <h2>Home</h2>
+      <div className="timeline__header">
+        <h2>ホーム</h2>
       </div>
       {/* TweetBox */}
 
       {/* Post */}
     </div>
-  )
+  );
 }
 
-export default Timeline
+export default Timeline;
