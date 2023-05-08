4f6b900130247c831d5185562c18ea6b804ed624
minntoko <minntoko0822@gmail.com>
2023-05-08 19:31:42 +0900

    add: Timelineの雛形を追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Sidevar from "./components/sidebar/Sidebar";
+import Timeline from "./components/timeline/Timeline";
 
 function App() {
   return (
     <div className="app">
       <Sidevar />
+      {/* Timeline */}
+      <Timeline />
     </div>
   );
 }
---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+      {/* Header */}
+      <div className='timeline__header'>
+        <h2>Home</h2>
+      </div>
+      {/* TweetBox */}
+
+      {/* Post */}
+    </div>
+  )
+}
+
+export default Timeline
