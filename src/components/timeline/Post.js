d5875562e93f38ac66ddef668d65d9c3ca430122
minntoko <minntoko0822@gmail.com>
2023-05-08 22:12:08 +0900

    add: PostのCSSを適用

--- src/components/timeline/Post.js
+++ src/components/timeline/Post.js
@@ -1,6 +1,7 @@
 import { ChatBubbleOutlineRounded, FavoriteBorder, FileUploadOutlined, Repeat, VerifiedUser } from '@mui/icons-material'
 import { Avatar } from '@mui/material'
 import React from 'react'
+import './Post.css'
 
 function Post() {
   return (
@@ -13,15 +14,15 @@ function Post() {
           <h3>ITエンジニア</h3>
           <span className='post__special'>
             <VerifiedUser className='post__badge' />
-            @itengineer
+            @it_engineer
           </span>
         </div>
         <div className='post__body'>
           <div className='post__description'>
             <p>Reactを勉強中です。</p>
           </div>
-          <div className='post_img'>
-            <img src="https://source.unsplash.com/random" />
+          <div className='post__img'>
+            <img src="https://images.unsplash.com/photo-1533986690673-c50390c01cfa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
           </div>
         </div>
         <div className='post__footer'>
