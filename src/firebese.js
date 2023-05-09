d0834d323711cf213a19c990110ac8ceaf18004d
minntoko <minntoko0822@gmail.com>
2023-05-09 20:43:49 +0900

    add: firebaseの設定を追加

---
+++ src/firebese.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
+
+const app = initializeApp(firebaseConfig);
