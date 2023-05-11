55ef4a4fcc9e257c9f5d238c7909dd6fc12651a6
minntoko <minntoko0822@gmail.com>
2023-05-11 17:41:54 +0900

    add: ツイート投稿機能

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,22 +1,51 @@
 import { Avatar, Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import db from "../../firebase";
 import "./TweetBox.css";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+    addDoc(collection(db, "post"), {
+      displayName: "ITエンジニア",
+      userName: "it_engineer",
+      verified: true,
+      text: tweetMessage,
+      avatar: "https://pbs.twimg.com/profile_images/1259823801967079425/EgoCaYUj.jpg",
+      image: tweetImage,
+    });
+    setTweetMessage("");
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar />
-          <input type="text" placeholder="いまどうしてる？" />
+          <input
+            type="text"
+            placeholder="いまどうしてる？"
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+          />
         </div>
         <div className="tweetBox__buttons">
           <input
             className="tweetBox__imageInput"
             type="text"
             placeholder="画像のURLを入力してください"
+            value={tweetImage}
+            onChange={(e) => setTweetImage(e.target.value)}
           />
-          <Button className="tweetBox__tweetButton" type="submit">
+          <Button
+            className="tweetBox__tweetButton"
+            type="submit"
+            onClick={sendTweet}
+          >
             ツイートする
           </Button>
         </div>
