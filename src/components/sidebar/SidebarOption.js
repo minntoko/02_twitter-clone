7e986dfc98ad57a4a417d91724cb0372334f773f
minntoko <minntoko0822@gmail.com>
2023-05-16 17:13:53 +0900

    fix: サイドバーのhoverの挙動を修正

--- src/components/sidebar/SidebarOption.js
+++ src/components/sidebar/SidebarOption.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import './SidebarOption.css'
+import React from "react";
+import "./SidebarOption.css";
 
 function SidebarOption({ text, Icon, active }) {
   return (
-    <div className={`sidebarOption ${active && "sidebarOption__active"}`}>
-      <Icon />
-      <h2>{ text }</h2>
+    <div className="sidebarOption__box">
+      <div className={`sidebarOption ${active && "sidebarOption__active"}`}>
+        <Icon />
+        <h2>{text}</h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SidebarOption
+export default SidebarOption;
