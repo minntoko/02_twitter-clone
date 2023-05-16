ae7759f5680be40312e1cf356412335a914c0b44
minntoko <minntoko0822@gmail.com>
2023-05-16 16:48:46 +0900

    add: いいねボタンとリツイートボタンの処理を追加

--- src/components/timeline/Post.js
+++ src/components/timeline/Post.js
@@ -1,9 +1,18 @@
-import { ChatBubbleOutlineRounded, FavoriteBorder, FileUploadOutlined, Repeat, VerifiedUser } from '@mui/icons-material'
-import { Avatar } from '@mui/material'
-import React, { forwardRef } from 'react';
+import { ChatBubbleOutlineRounded, FavoriteBorder, FileUploadOutlined, Repeat, VerifiedUser } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
+import React, { useState, forwardRef } from 'react';
 import './Post.css'
 
 const Post = forwardRef(({ displayName, userName, verified, text, avatar, image }, ref) => {
+  // useStateでいいねの状態を保持
+  const [like, setLike] = useState(false);
+  const [retweet, setRetweet] = useState(false);
+  const likeSwitch = () => {
+    setLike(like ? false : true);
+  }
+  const retweetSwitch = () => {
+    setRetweet(retweet ? false : true);
+  }
   return (
     <div className='post' ref={ref}>
       <div className='post__avatar'>
@@ -27,8 +36,16 @@ const Post = forwardRef(({ displayName, userName, verified, text, avatar, image 
         </div>
         <div className='post__footer'>
           <ChatBubbleOutlineRounded fontSize='small' />
-          <Repeat fontSize='small' />
-          <FavoriteBorder fontSize='small' />
+          <Repeat
+            fontSize='small'
+            className={`post__retweet ${retweet ? "post__retweetActive" : ""}`}
+            onClick={retweetSwitch}
+          />
+            <FavoriteBorder
+              fontSize="small"
+              className={`post__like ${like ? "post__likeActive" : ""}`}
+              onClick={likeSwitch}
+            />
           <FileUploadOutlined fontSize='small' />
         </div>
       </div>
