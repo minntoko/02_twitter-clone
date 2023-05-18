28e2c4703b94ddc39b0ce75cde9cbaeab48c3b13
minntoko <minntoko0822@gmail.com>
2023-05-18 14:47:13 +0900

    add: いいねの数やリツイートの数を表示

--- src/components/timeline/buttons/LikeButton.js
+++ src/components/timeline/buttons/LikeButton.js
@@ -4,18 +4,30 @@ import { useState } from "react";
 import "./Buttons.css";
 
 const LikeButton = ({ like, likeSwitch }) => {
+  const [likeCount, setLikeCount] = useState(0);
+  const likeCountUp = () => {
+    if (like) {
+      setLikeCount(likeCount - 1);
+    } else {
+      setLikeCount(likeCount + 1);
+    }
+  };
   return (
-    <div className="likeBox">
-      <div
-        className={`post__likeButton ${like ? "post__likeActive" : ""}`}
-        onClick={likeSwitch}
-      >
+    <div
+      className="likeBox"
+      onClick={() => {
+        likeSwitch();
+        likeCountUp();
+      }}
+    >
+      <div className={`post__likeButton ${like ? "post__likeActive" : ""}`}>
         {like ? (
           <FavoriteIcon fontSize="small" />
         ) : (
           <FavoriteBorder fontSize="small" />
         )}
       </div>
+      <span className={`likeButton__likeCount ${likeCount !== 0 ? "likeButton__likeActive" : ""}`}>{likeCount}</span>
     </div>
   );
 };
