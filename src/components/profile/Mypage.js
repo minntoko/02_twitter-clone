c06708b80e5d2d5adec574e0e2ca95608a3040af
minntoko <minntoko0822@gmail.com>
2023-05-22 20:54:16 +0900

    add: プロフィールのヘッダーを追加

--- src/components/profile/Mypage.js
+++ src/components/profile/Mypage.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import './Mypage.css';
+import React from "react";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { Link } from "react-router-dom";
+import "./Mypage.css";
+import ProfArea from "./ProfArea";
 
 function Mypage() {
   return (
-    <div className='mypage'>Mypage</div>
-  )
+    <div className="mypage">
+      <div className="mypage__header">
+        <Link to="/" className="mypage__backBtn">
+          <KeyboardBackspaceIcon />
+        </Link>
+        <div className="mypage__displayName">
+          <h2>ITエンジニア</h2>
+          <span>123件のツイート</span>
+        </div>
+      </div>
+      <ProfArea />
+    </div>
+  );
 }
 
-export default Mypage
+export default Mypage;
---
+++ src/components/profile/ProfArea.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProfArea() {
+  return (
+    <div>ProfArea</div>
+  )
+}
+
+export default ProfArea
