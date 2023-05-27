4c18030591685f0a074ddd34558085552050bd73
minntoko <minntoko0822@gmail.com>
2023-05-27 23:38:53 +0900

    feat: coverとiconを作成

--- src/components/profile/ProfArea.js
+++ src/components/profile/ProfArea.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import './ProfArea.css'
+import { memo, useContext } from "react";
+import { UserDataContext } from "../providers/userDataProvider";
+import "./ProfArea.css";
 
-function ProfArea() {
+const ProfArea = memo(() => {
+  const { userData } = useContext(UserDataContext);
   return (
-    <div className='profArea'>
-      <div className='profArea__cover'>
-        <span className='profArea__span'>profArea</span>
+    <div className="profArea">
+      <img src={`${userData.cover}`} alt="cover" className="profArea__cover" />
+      <div className="profArea__prof">
+        <div className="profArea__iconBox">
+          <img src={`${userData.icon}`} alt="icon" className="profArea__icon" />
+        </div>
+        <h2 className="profArea__displayName">{userData.displayName}</h2>
+        <span>@{userData.userId}</span>
       </div>
-      <div className='profArea__icon'></div>
     </div>
-  )
-}
+  );
+});
 
-export default ProfArea
+export default ProfArea;
