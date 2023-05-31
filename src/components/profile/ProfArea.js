cff270e78c31d58dd8137a875e4561982cb70e5e
minntoko <minntoko0822@gmail.com>
2023-06-01 01:48:31 +0900

    fert: 自己紹介を追加

--- src/components/profile/ProfArea.js
+++ src/components/profile/ProfArea.js
@@ -1,22 +1,33 @@
-import { memo, useContext } from "react";
+import { memo, useContext, useEffect } from "react";
 import { Avatar, Button } from "@mui/material";
 import { UserDataContext } from "../providers/userDataProvider";
 import "./ProfArea.css";
+import { useState } from "react";
 
 const ProfArea = memo(() => {
   const { userData } = useContext(UserDataContext);
+  const formatDescription = (description) => {
+    if (!description) return null;
+    const lines = description.split('\\n');
+    return lines.map((line, index) => <span key={index} style={{ display: 'block' }}>{line}</span>);
+  };
   return (
     <div className="profArea">
       <img src={`${userData.cover}`} alt="cover" className="profArea__cover" />
       <div className="profArea__prof">
         <img src={`${userData.icon}`} alt="icon" className="profArea__icon" />
         <div className="profArea__iconBox">
-          <div>
-            <Button className="profArea__followButton">プロフィールを編集</Button>
-          </div>
+          <Button className="profArea__followButton">プロフィールを編集</Button>
         </div>
-        <h2 className="profArea__displayName">{userData.displayName}</h2>
-        <span>@{userData.userId}</span>
+        <div className="profArea__nameArea">
+          <h2 className="profArea__displayName">{userData.displayName}</h2>
+          <span>@{userData.userId}</span>
+        </div>
+        <div className="profArea__desp">
+          <p>
+            {formatDescription(userData.description)}
+          </p>
+        </div>
       </div>
     </div>
   );
